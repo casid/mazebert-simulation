8d626eee100a018aa80ebcb06fbd60b3cf979c4c
casid <andy@mazebert.com>
2018-11-02 23:48:08 +0100

    load map tiles

--- src/main/java/com/mazebert/simulation/gateways/GameGateway.java
+++ src/main/java/com/mazebert/simulation/gateways/GameGateway.java
@@ -1,6 +1,7 @@
 package com.mazebert.simulation.gateways;
 
 import com.mazebert.simulation.Game;
+import com.mazebert.simulation.maps.Map;
 import org.jusecase.inject.Component;
 
 @Component
@@ -9,5 +10,9 @@ public strictfp class GameGateway {
 
     public Game getGame() {
         return game;
+    }
+
+    public Map getMap() {
+        return game.map;
     }
 }
