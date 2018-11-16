9093e14cbbf284a803fb74f2bc325d7e6df9b98d
casid <andy@mazebert.com>
2018-11-16 20:09:59 +0100

    Add tower sanity test

---
+++ src/test/java/com/mazebert/simulation/units/towers/TowerTypeTest.java
@@ -0,0 +1,31 @@
+package com.mazebert.simulation.units.towers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class TowerTypeTest {
+    @Test
+    void uniqueIds() {
+        Map<Integer, TowerType> ids = new HashMap<>();
+        for (TowerType type : TowerType.values()) {
+            TowerType existing = ids.get(type.id);
+            if (existing != null) {
+                fail("Please choose a unique id for tower " + type + " (the id " + existing.id + " is already taken by " + existing + ")");
+            }
+            ids.put(type.id, type);
+        }
+    }
+
+    @Test
+    void allRequiredInfosAreSet() {
+        for (TowerType type : TowerType.values()) {
+            Tower tower = type.create();
+            assertThat(tower.getElement()).describedAs("Tower " + type + " has no element").isNotNull();
+        }
+    }
+}
