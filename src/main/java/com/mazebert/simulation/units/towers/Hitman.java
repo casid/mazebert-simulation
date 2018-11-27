d014e4d131c9c22ffb1ef3d265d6bdb5ff422d4a
Andreas Hager <andy@mazebert.com>
2018-11-27 14:39:43 +0100

    Fix hitman damage

--- src/main/java/com/mazebert/simulation/units/towers/Hitman.java
+++ src/main/java/com/mazebert/simulation/units/towers/Hitman.java
@@ -3,9 +3,12 @@ package com.mazebert.simulation.units.towers;
 import com.mazebert.simulation.AttackType;
 import com.mazebert.simulation.Element;
 import com.mazebert.simulation.Rarity;
+import com.mazebert.simulation.projectiles.ProjectileViewType;
 import com.mazebert.simulation.units.Gender;
 import com.mazebert.simulation.units.abilities.AttackAbility;
+import com.mazebert.simulation.units.abilities.DamageAbility;
 import com.mazebert.simulation.units.abilities.DelayedDamageAbility;
+import com.mazebert.simulation.units.abilities.ProjectileDamageAbility;
 
 public strictfp class Hitman extends Tower {
     public Hitman() {
@@ -18,7 +21,7 @@ public strictfp class Hitman extends Tower {
         setElement(Element.Metropolis);
 
         addAbility(new AttackAbility());
-        addAbility(new DelayedDamageAbility(1.0f));
+        addAbility(new DamageAbility());
         addAbility(new HitmanHeadshotAbility());
     }
 
