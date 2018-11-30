93cf9df9f19fcdf21d9adfadc668474578ab3e07
Andreas Hager <andy@mazebert.com>
2018-11-30 13:15:53 +0100

    Add find method

--- src/main/java/com/mazebert/simulation/util/SafeIterationArray.java
+++ src/main/java/com/mazebert/simulation/util/SafeIterationArray.java
@@ -1,10 +1,11 @@
 package com.mazebert.simulation.util;
 
 import com.mazebert.java8.Consumer;
+import com.mazebert.java8.Predicate;
 
 import java.util.Arrays;
 
-public class SafeIterationArray<T> {
+public strictfp class SafeIterationArray<T> {
     private static final int LOAD_FACTOR = 2;
 
     private Object[] elements;
@@ -54,12 +55,31 @@ public class SafeIterationArray<T> {
     @SuppressWarnings("unchecked")
     public void forEach(Consumer<T> consumer) {
         pushIteration();
-        for (int i = 0; i < size; ++i) {
-            if (elements[i] != null) {
-                consumer.accept((T) elements[i]);
+        try {
+            for (int i = 0; i < size; ++i) {
+                if (elements[i] != null) {
+                    consumer.accept((T) elements[i]);
+                }
             }
+        } finally {
+            popIteration();
         }
-        popIteration();
+    }
+
+    @SuppressWarnings("unchecked")
+    public T find(Predicate<T> predicate) {
+        pushIteration();
+        try {
+            for (int i = 0; i < size; ++i) {
+                if (elements[i] != null && predicate.test((T) elements[i]) ) {
+                    return (T) elements[i];
+                }
+            }
+        } finally {
+            popIteration();
+        }
+
+        return null;
     }
 
     private void pushIteration() {
