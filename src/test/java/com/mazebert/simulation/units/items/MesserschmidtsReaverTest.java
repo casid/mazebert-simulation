c3508c18666e00d6c391227ce2d68b3393ff87d8
Andreas Hager <andy@mazebert.com>
2018-12-14 03:36:16 +0100

    be sure this works

--- src/test/java/com/mazebert/simulation/units/items/MesserschmidtsReaverTest.java
+++ src/test/java/com/mazebert/simulation/units/items/MesserschmidtsReaverTest.java
@@ -6,9 +6,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MesserschmidtsReaverTest extends ItemTest {
     @Test
-    void adjustments() {
+    void rangeIsReduced() {
         tower.setBaseRange(4);
         whenItemIsEquipped(ItemType.MesserschmidtsReaver);
         assertThat(tower.getRange()).isEqualTo(2.4f);
+    }
+
+    @Test
+    void rangeIsReduced_notBelow1() {
+        tower.setBaseRange(1);
+        whenItemIsEquipped(ItemType.MesserschmidtsReaver);
+        assertThat(tower.getRange()).isEqualTo(1);
     }
 }
