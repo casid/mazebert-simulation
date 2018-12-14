d07db820f4b3b625cc3ade89a8bdaa1276e1edcd
Andreas Hager <andy@mazebert.com>
2018-12-14 03:05:56 +0100

    notifications

--- src/main/java/com/mazebert/simulation/units/towers/GibFreezeEffect.java
+++ src/main/java/com/mazebert/simulation/units/towers/GibFreezeEffect.java
@@ -1,19 +1,31 @@
 package com.mazebert.simulation.units.towers;
 
+import com.mazebert.simulation.Sim;
+import com.mazebert.simulation.SimulationListeners;
 import com.mazebert.simulation.units.creeps.Creep;
 import com.mazebert.simulation.units.creeps.effects.SlowEffect;
 
 public class GibFreezeEffect extends SlowEffect {
-
-    @Override
-    protected void dispose(Creep unit) {
-        unit.addArmor(-getStackCount());
-        super.dispose(unit);
-    }
+    private final SimulationListeners simulationListeners = Sim.context().simulationListeners;
 
     @Override
     public void addStack(float slowMultiplier, float duration) {
         super.addStack(slowMultiplier, duration);
         getUnit().addArmor(1);
+
+        if (simulationListeners.areNotificationsEnabled()) {
+            simulationListeners.showNotification(getUnit(), "Frozen!", 0x3355aa);
+        }
     }
+
+    @Override
+    protected void dispose(Creep unit) {
+        if (simulationListeners.areNotificationsEnabled()) {
+            simulationListeners.showNotification(getUnit(), "Melted!", 0x3355aa);
+        }
+
+        unit.addArmor(-getStackCount());
+        super.dispose(unit);
+    }
+
 }
