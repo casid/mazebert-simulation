4bf2448673e28fb064eb50e0f776023b08cc4472
casid <andy@mazebert.com>
2018-12-23 18:57:41 +0100

    bugfix

--- src/main/java/com/mazebert/simulation/listeners/OnResurrect.java
+++ src/main/java/com/mazebert/simulation/listeners/OnResurrect.java
@@ -5,6 +5,6 @@ import com.mazebert.simulation.units.creeps.Creep;
 public strictfp class OnResurrect extends ExposedSignal<OnResurrectListener> {
 
     public void dispatch(Creep target) {
-        dispatchExposed(l -> l.onResurrect(target));
+        dispatchAll(l -> l.onResurrect(target));
     }
 }
