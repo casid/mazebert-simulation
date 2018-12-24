6e1276911ec419042484a1bee8bf7a982e2ffd5f
casid <andy@mazebert.com>
2018-12-24 23:13:31 +0100

    add text

--- src/main/java/com/mazebert/simulation/units/heroes/LittleFingerAbility.java
+++ src/main/java/com/mazebert/simulation/units/heroes/LittleFingerAbility.java
@@ -3,9 +3,29 @@ package com.mazebert.simulation.units.heroes;
 import com.mazebert.simulation.units.towers.Tower;
 
 public strictfp class LittleFingerAbility extends HeroTowerBuffAbility {
+
+    public static final float itemChance = 0.2f;
+    public static final float itemQuality = 0.2f;
+
     @Override
     protected void buffTower(Tower tower) {
-        tower.addItemChance(0.2f);
-        tower.addItemQuality(0.2f);
+        tower.addItemChance(itemChance);
+        tower.addItemQuality(itemQuality);
+    }
+
+    @Override
+    public boolean isVisibleToUser() {
+        return true;
+    }
+
+    @Override
+    public String getTitle() {
+        return "The little difference.";
+    }
+
+    @Override
+    public String getDescription() {
+        return format.percentWithSignAndUnit(itemChance) + " item chance\n" +
+                format.percentWithSignAndUnit(itemQuality) + " item quality";
     }
 }
