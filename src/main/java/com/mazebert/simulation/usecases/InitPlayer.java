d0965b1172417f61139bb2687fe83c46f1ab2cc6
casid <andy@mazebert.com>
2018-12-24 22:50:59 +0100

    init hero

--- src/main/java/com/mazebert/simulation/usecases/InitPlayer.java
+++ src/main/java/com/mazebert/simulation/usecases/InitPlayer.java
@@ -1,12 +1,25 @@
 package com.mazebert.simulation.usecases;
 
+import com.mazebert.simulation.Sim;
 import com.mazebert.simulation.commands.InitPlayerCommand;
+import com.mazebert.simulation.gateways.UnitGateway;
+import com.mazebert.simulation.units.heroes.Hero;
+import com.mazebert.simulation.units.heroes.HeroType;
 
 public strictfp class InitPlayer extends Usecase<InitPlayerCommand> {
 
+    private final UnitGateway unitGateway = Sim.context().unitGateway;
+
     @Override
     public void execute(InitPlayerCommand command) {
+        addHero(command);
+    }
 
+    private void addHero(InitPlayerCommand command) {
+        HeroType heroType = HeroType.forId(command.heroId);
+        Hero hero = heroType.create();
+        hero.setWizard(unitGateway.getWizard(command.playerId));
+        unitGateway.addUnit(hero);
     }
 
 }
---
+++ src/test/java/com/mazebert/simulation/usecases/InitPlayerTest.java
@@ -0,0 +1,40 @@
+package com.mazebert.simulation.usecases;
+
+import com.mazebert.simulation.SimulationListeners;
+import com.mazebert.simulation.commands.InitPlayerCommand;
+import com.mazebert.simulation.gateways.UnitGateway;
+import com.mazebert.simulation.units.heroes.HeroType;
+import com.mazebert.simulation.units.heroes.LittleFinger;
+import com.mazebert.simulation.units.wizards.Wizard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+strictfp class InitPlayerTest extends UsecaseTest<InitPlayerCommand> {
+    Wizard wizard;
+
+    @BeforeEach
+    void setUp() {
+        simulationListeners = new SimulationListeners();
+        unitGateway = new UnitGateway();
+
+        wizard = new Wizard();
+        wizard.playerId = 1;
+        unitGateway.addUnit(wizard);
+
+        usecase = new InitPlayer();
+
+        request.playerId = wizard.getPlayerId();
+    }
+
+    @Test
+    void littlefinger() {
+        request.heroId = HeroType.LittleFinger.id;
+
+        whenRequestIsExecuted();
+
+        LittleFinger hero = unitGateway.findUnit(LittleFinger.class, wizard.playerId);
+        assertThat(hero).isNotNull();
+    }
+}
