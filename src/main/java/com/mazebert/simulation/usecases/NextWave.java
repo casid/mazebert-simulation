8df2f2bfc0e9a64e92beb90a15e0c0ebfc383f3a
Andreas Hager <andy@mazebert.com>
2019-01-07 06:41:01 +0100

    bonus round countdown can be skipped by next wave button

--- src/main/java/com/mazebert/simulation/usecases/NextWave.java
+++ src/main/java/com/mazebert/simulation/usecases/NextWave.java
@@ -6,9 +6,11 @@ import com.mazebert.simulation.SimulationListeners;
 import com.mazebert.simulation.commands.NextWaveCommand;
 import com.mazebert.simulation.countdown.CountDown;
 import com.mazebert.simulation.countdown.EarlyCallCountDown;
+import com.mazebert.simulation.gateways.GameGateway;
 
 public strictfp class NextWave extends Usecase<NextWaveCommand> {
     private final SimulationListeners simulationListeners = Sim.context().simulationListeners;
+    private final GameGateway gameGateway = Sim.context().gameGateway;
 
     @Override
     public void execute(NextWaveCommand command) {
@@ -17,10 +19,17 @@ public strictfp class NextWave extends Usecase<NextWaveCommand> {
             return;
         }
 
+        if (gameGateway.getGame().bonusRound) {
+            if (context.bonusRoundCountDown != null) {
+                skipCountDown(context, context.bonusRoundCountDown);
+            }
+            return;
+        }
+
         if (context.gameCountDown != null) {
             skipCountDown(context, context.gameCountDown);
         } else if (context.waveCountDown != null) {
-            skipCountDown(context,context.waveCountDown);
+            skipCountDown(context, context.waveCountDown);
         } else {
             simulationListeners.onWaveStarted.dispatch();
         }
--- src/test/java/com/mazebert/simulation/usecases/NextWaveTest.java
+++ src/test/java/com/mazebert/simulation/usecases/NextWaveTest.java
@@ -2,7 +2,9 @@ package com.mazebert.simulation.usecases;
 
 import com.mazebert.simulation.SimulationListeners;
 import com.mazebert.simulation.commands.NextWaveCommand;
+import com.mazebert.simulation.countdown.BonusRoundCountDown;
 import com.mazebert.simulation.countdown.WaveCountDown;
+import com.mazebert.simulation.gateways.GameGateway;
 import com.mazebert.simulation.gateways.WaveGateway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,15 +13,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class NextWaveTest extends UsecaseTest<NextWaveCommand> {
     private int waveStarted;
+    private int bonusRoundStarted;
 
     @BeforeEach
     void setUp() {
         simulationListeners = new SimulationListeners();
         waveGateway = new WaveGateway();
+        gameGateway = new GameGateway();
 
         usecase = new NextWave();
 
         simulationListeners.onWaveStarted.add(() -> ++waveStarted);
+        simulationListeners.onBonusRoundStarted.add(() -> ++bonusRoundStarted);
     }
 
     @Test
@@ -33,13 +38,24 @@ class NextWaveTest extends UsecaseTest<NextWaveCommand> {
         whenRequestIsExecuted();
         whenRequestIsExecuted();
 
-        assertThat(waveStarted).isEqualTo(1); // ignored
+        assertThat(waveStarted).isEqualTo(1); // Extra command is ignored
     }
 
     @Test
     void skippedSeconds() {
         waveCountDown = new WaveCountDown();
-        whenErrorRequestIsExecuted();
+        whenRequestIsExecuted();
         assertThat(skippedSeconds).isEqualTo(5);
+    }
+
+    @Test
+    void bonusRound() {
+        gameGateway.getGame().bonusRound = true;
+        bonusRoundCountDown = new BonusRoundCountDown();
+
+        whenRequestIsExecuted();
+
+        assertThat(bonusRoundStarted).isEqualTo(1);
+        assertThat(skippedSeconds).isEqualTo(60);
     }
 }
