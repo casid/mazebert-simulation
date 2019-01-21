10fe924dc0a283dc8c8c14f95cf5500d93a35079
Andreas Hager <andy@mazebert.com>
2019-01-21 10:41:50 +0100

    Add game params to poke

--- src/main/java/com/mazebert/simulation/nat/messages/NatTraversalPoke.java
+++ src/main/java/com/mazebert/simulation/nat/messages/NatTraversalPoke.java
@@ -2,7 +2,12 @@ package com.mazebert.simulation.nat.messages;
 
 import org.jusecase.bitnet.message.BitMessage;
 
+import java.util.UUID;
+
 public class NatTraversalPoke extends BitMessage {
+    public UUID gameId;
+    public long playerId1;
+    public long playerId2;
     public long localSecret;
     public long remoteSecret;
 }
--- src/main/java/com/mazebert/simulation/nat/serializers/NatTraversalPokeSerializer.java
+++ src/main/java/com/mazebert/simulation/nat/serializers/NatTraversalPokeSerializer.java
@@ -13,12 +13,18 @@ public class NatTraversalPokeSerializer implements BitSerializer<NatTraversalPok
 
     @Override
     public void serialize(BitWriter bitWriter, NatTraversalPoke object) {
+        bitWriter.writeUuidNonNull(object.gameId);
+        bitWriter.writeLong(object.playerId1);
+        bitWriter.writeLong(object.playerId2);
         bitWriter.writeLong(object.localSecret);
         bitWriter.writeLong(object.remoteSecret);
     }
 
     @Override
     public void deserialize(BitReader bitReader, NatTraversalPoke object) {
+        object.gameId = bitReader.readUuidNonNull();
+        object.playerId1 = bitReader.readLong();
+        object.playerId2 = bitReader.readLong();
         object.localSecret = bitReader.readLong();
         object.remoteSecret = bitReader.readLong();
     }
