4924c31da7e27b44e462aa25293e24e17a1a7047
casid <andy@mazebert.com>
2019-01-23 02:11:56 +0100

    byte order when looking at output

--- src/main/java/com/mazebert/simulation/replay/data/ReplayHeader.java
+++ src/main/java/com/mazebert/simulation/replay/data/ReplayHeader.java
@@ -3,7 +3,7 @@ package com.mazebert.simulation.replay.data;
 import com.mazebert.simulation.Sim;
 
 public strictfp class ReplayHeader {
-    public static final int FORMAT_IDENTIFIER = 0x6d626773; // m b g s
+    public static final int FORMAT_IDENTIFIER = 0x7367626d; // m b g s
 
     public int version = Sim.version;
     public int playerId;
