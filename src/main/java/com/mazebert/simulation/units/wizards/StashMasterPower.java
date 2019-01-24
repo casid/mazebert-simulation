b3b689984909d80e27fcbdb2d3bd0fada1fd2818
casid <andy@mazebert.com>
2019-01-24 08:44:56 +0100

    Add stash master power

---
+++ src/main/java/com/mazebert/simulation/units/wizards/StashMasterPower.java
@@ -0,0 +1,54 @@
+package com.mazebert.simulation.units.wizards;
+
+import com.mazebert.simulation.Rarity;
+import com.mazebert.simulation.units.towers.Tower;
+
+public class StashMasterPower extends WizardTowerBuffPower {
+
+    @Override
+    protected void buffTower(Tower tower) {
+        if (tower.getRarity().ordinal() > getRarity().ordinal()) {
+            return;
+        }
+
+        tower.addInventorySize(1);
+    }
+
+    public Rarity getRarity() {
+        int skillLevel = getSkillLevel();
+        if (skillLevel <= 2) {
+            return Rarity.Common;
+        } else if (skillLevel <= 4) {
+            return Rarity.Uncommon;
+        } else if (skillLevel <= 6) {
+            return Rarity.Rare;
+        } else if (skillLevel <= 9) {
+            return Rarity.Unique;
+        }
+        return Rarity.Legendary;
+    }
+
+    @Override
+    public int getRequiredLevel() {
+        return 50;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Stash Master";
+    }
+
+    @Override
+    public String getDescription() {
+        String suffix = ".";
+        if (getRarity() != Rarity.Common) {
+            suffix = " and below.";
+        }
+        return "+ 1 inventory slot\n" + format.rarity(getRarity(), false) + " towers" + suffix;
+    }
+
+    @Override
+    public String getIconFile() {
+        return "0040_holyman_512";
+    }
+}
---
+++ src/test/java/com/mazebert/simulation/units/wizards/StashMasterPowerTest.java
@@ -0,0 +1,64 @@
+package com.mazebert.simulation.units.wizards;
+
+import com.mazebert.simulation.SimTest;
+import com.mazebert.simulation.SimulationListeners;
+import com.mazebert.simulation.gateways.UnitGateway;
+import com.mazebert.simulation.units.towers.Manitou;
+import com.mazebert.simulation.units.towers.Rabbit;
+import com.mazebert.simulation.units.towers.Tower;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+strictfp class StashMasterPowerTest extends SimTest {
+    Wizard wizard;
+    StashMasterPower power;
+    Tower tower;
+
+    @BeforeEach
+    void setUp() {
+        simulationListeners = new SimulationListeners();
+        unitGateway = new UnitGateway();
+
+        wizard = new Wizard();
+        wizard.playerId = 1;
+        power = new StashMasterPower();
+        power.setSkillLevel(10);
+
+        wizard.level = power.getRequiredLevel();
+        wizard.addAbility(power);
+        unitGateway.addUnit(wizard);
+
+        tower = new Manitou();
+    }
+
+    @Test
+    void towerCardIsAdded() {
+        whenTowerIsAdded();
+        assertThat(tower.getInventorySize()).isEqualTo(5);
+    }
+
+    @Test
+    void skillLevelTooLowForTower() {
+        power.setSkillLevel(5);
+        whenTowerIsAdded();
+        assertThat(tower.getInventorySize()).isEqualTo(4);
+    }
+
+    @Test
+    void level1() {
+        tower = new Rabbit();
+        power.setSkillLevel(1);
+
+        whenTowerIsAdded();
+
+        assertThat(tower.getInventorySize()).isEqualTo(5);
+    }
+
+    private void whenTowerIsAdded() {
+        tower.setWizard(wizard);
+        unitGateway.addUnit(tower);
+    }
+
+}
