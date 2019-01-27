2da1263685a4a6e9af96077df057b848c6934516
casid <andy@mazebert.com>
2019-01-27 20:45:30 +0100

    Extract public api classes

--- src/main/java/com/mazebert/simulation/plugins/random/UuidRandomPlugin.java
+++ src/main/java/com/mazebert/simulation/plugins/random/UuidRandomPlugin.java
@@ -7,21 +7,9 @@ public strictfp class UuidRandomPlugin extends RandomPlugin {
     private FastRandomPlugin[] fastRandom = new FastRandomPlugin[4];
     private int nextIndex;
 
-    public static UUID createSeed() {
-        UUID uuid;
-        int[] seeds;
-
-        do {
-            uuid = UUID.randomUUID();
-            seeds = extractSeeds(uuid);
-        } while (!validSeeds(seeds));
-
-        return uuid;
-    }
-
     public void setSeed(UUID seed) {
-        int[] seeds = extractSeeds(seed);
-        if (validSeeds(seeds)) {
+        int[] seeds = GameIdGenerator.extractSeeds(seed);
+        if (GameIdGenerator.validSeeds(seeds)) {
             for (int i = 0; i < fastRandom.length; ++i) {
                 fastRandom[i] = new FastRandomPlugin();
                 fastRandom[i].setSeed(seeds[i]);
@@ -29,29 +17,6 @@ public strictfp class UuidRandomPlugin extends RandomPlugin {
         } else {
             throw new IllegalStateException("Random seeds must not contain 0x00000000!");
         }
-    }
-
-    private static boolean validSeeds(int[] seeds) {
-        for (int seed : seeds) {
-            if (seed == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static int[] extractSeeds(UUID uuid) {
-        int[] seeds = new int[4];
-
-        long a = uuid.getMostSignificantBits();
-        long b = uuid.getLeastSignificantBits();
-
-        seeds[0] = (int)(a >>> 32);
-        seeds[1] = (int)(a);
-        seeds[2] = (int)(b >>> 32);
-        seeds[3] = (int)(b);
-
-        return seeds;
     }
 
     int getInternalSeed(int index) {
