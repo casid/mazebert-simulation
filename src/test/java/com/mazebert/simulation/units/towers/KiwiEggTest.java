0a6df42a537b01d9d0d74ccfebeced8d41153b07
Andreas Hager <andy@mazebert.com>
2019-02-07 22:11:46 +0100

    Add aura test for hatching kiwi

--- src/test/java/com/mazebert/simulation/units/towers/KiwiEggTest.java
+++ src/test/java/com/mazebert/simulation/units/towers/KiwiEggTest.java
@@ -3,7 +3,10 @@ package com.mazebert.simulation.units.towers;
 import com.mazebert.simulation.SimTest;
 import com.mazebert.simulation.SimulationListeners;
 import com.mazebert.simulation.Wave;
+import com.mazebert.simulation.gateways.GameGateway;
 import com.mazebert.simulation.gateways.UnitGateway;
+import com.mazebert.simulation.maps.MapAura;
+import com.mazebert.simulation.maps.TestMap;
 import com.mazebert.simulation.plugins.random.RandomPluginTrainer;
 import com.mazebert.simulation.units.Gender;
 import com.mazebert.simulation.units.wizards.Wizard;
@@ -15,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 strictfp class KiwiEggTest extends SimTest {
     RandomPluginTrainer randomPluginTrainer = new RandomPluginTrainer();
 
+    TestMap map;
     Wizard wizard;
     KiwiEgg kiwiEgg;
     Kiwi kiwi;
@@ -23,7 +27,11 @@ strictfp class KiwiEggTest extends SimTest {
     void setUp() {
         simulationListeners = new SimulationListeners();
         unitGateway = new UnitGateway();
+        gameGateway = new GameGateway();
         randomPlugin = randomPluginTrainer;
+
+        map = new TestMap(1);
+        gameGateway.getGame().map = map;
 
         wizard = new Wizard();
         unitGateway.addUnit(wizard);
@@ -72,6 +80,13 @@ strictfp class KiwiEggTest extends SimTest {
         randomPluginTrainer.givenFloatAbs(0.5f);
         whenKiwiHatches();
         assertThat(kiwi.getGender()).isEqualTo(Gender.Male);
+    }
+
+    @Test
+    void hatches_aura() {
+        map.givenAllTilesHaveAura(MapAura.IncreasedRange);
+        whenKiwiHatches();
+        assertThat(kiwi.getRange()).isEqualTo(2);
     }
 
     private void whenKiwiHatches() {
