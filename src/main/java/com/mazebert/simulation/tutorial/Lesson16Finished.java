fd0cfb90095f3bd9083205f7b80d3fa1d552be1b
casid <andy@mazebert.com>
2019-02-26 23:06:35 +0100

    Better finish

--- src/main/java/com/mazebert/simulation/tutorial/Lesson16Finished.java
+++ src/main/java/com/mazebert/simulation/tutorial/Lesson16Finished.java
@@ -1,5 +1,27 @@
 package com.mazebert.simulation.tutorial;
 
-public strictfp class Lesson16Finished extends Lesson07SecondWave {
+import com.mazebert.simulation.Sim;
+import com.mazebert.simulation.listeners.OnPauseListener;
+import com.mazebert.simulation.units.wizards.Wizard;
 
+public strictfp class Lesson16Finished extends Lesson07SecondWave implements OnPauseListener {
+
+    @Override
+    public void initialize(Wizard wizard) {
+        super.initialize(wizard);
+        Sim.context().simulationListeners.onPause.add(this);
+    }
+
+    @Override
+    public void dispose(Wizard wizard) {
+        Sim.context().simulationListeners.onPause.remove(this);
+        super.dispose(wizard);
+    }
+
+    @Override
+    public void onPause(int playerId, boolean pause) {
+        if (!pause) {
+            finish();
+        }
+    }
 }
