c376854a72dec356015895d32336455cfe236591
Andreas Hager <andy@mazebert.com>
2019-08-08 07:21:06 +0200

    Make onMiss exposed to client

--- src/main/java/com/mazebert/simulation/listeners/OnMiss.java
+++ src/main/java/com/mazebert/simulation/listeners/OnMiss.java
@@ -1,12 +1,15 @@
 package com.mazebert.simulation.listeners;
 
 import com.mazebert.simulation.units.creeps.Creep;
-import org.jusecase.signals.Signal;
 
-public strictfp class OnMiss extends Signal<OnMissListener> {
+public strictfp class OnMiss extends ExposedSignal<OnMissListener> {
     public void dispatch(Object origin, Creep target) {
         for (OnMissListener listener : this) {
             listener.onMiss(origin, target);
+        }
+
+        if (isExposed()) {
+            dispatchExposed(l -> l.onMiss(origin, target));
         }
     }
 }
