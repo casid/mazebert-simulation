94e2cb4fd9d2f5f1636889149e6b23498fd16feb
Andreas Hager <andy@mazebert.com>
2019-08-28 18:30:25 +0200

    Fix quest title

--- src/main/java/com/mazebert/simulation/units/quests/OnlyMetropolisQuest.java
+++ src/main/java/com/mazebert/simulation/units/quests/OnlyMetropolisQuest.java
@@ -16,6 +16,6 @@ public strictfp class OnlyMetropolisQuest extends RestrictedElementsQuest {
 
     @Override
     public String getTitle() {
-        return "Nature's Wrath";
+        return "Urbanize!";
     }
 }
