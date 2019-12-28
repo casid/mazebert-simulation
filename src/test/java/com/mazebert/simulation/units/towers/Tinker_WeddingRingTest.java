98e60511b4c1359fd95b1436597b12c0a3a694f5
casid <andy@mazebert.com>
2019-12-28 05:40:08 +0100

    Add test to ensure tinker works with wedding rings

---
+++ src/test/java/com/mazebert/simulation/units/towers/Tinker_WeddingRingTest.java
@@ -0,0 +1,61 @@
+package com.mazebert.simulation.units.towers;
+
+import com.mazebert.simulation.systems.WeddingRingSystem;
+import com.mazebert.simulation.units.Gender;
+import com.mazebert.simulation.units.TestTower;
+import com.mazebert.simulation.units.items.ItemTest;
+import com.mazebert.simulation.units.items.ItemType;
+import com.mazebert.simulation.units.potions.PotionType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public strictfp class Tinker_WeddingRingTest extends ItemTest {
+
+    Tinker tinker;
+    TestTower otherTower;
+
+    @BeforeEach
+    void setUp() {
+        weddingRingSystem = new WeddingRingSystem();
+
+        otherTower = new TestTower();
+        otherTower.setWizard(wizard);
+        unitGateway.addUnit(otherTower);
+
+        whenItemIsEquipped(tinker, ItemType.WeddingRing1);
+        whenItemIsEquipped(otherTower, ItemType.WeddingRing2);
+
+        tinker.simulate(WeddingRingSystem.SECONDS_FOR_MARRIAGE);
+        otherTower.simulate(WeddingRingSystem.SECONDS_FOR_MARRIAGE);
+    }
+
+    @Override
+    protected Tower createTower() {
+        return tinker = new Tinker();
+    }
+
+    @Test
+    void initialLuck() {
+        assertThat(tinker.getLuck()).isEqualTo(1.05f);
+    }
+
+    @Test
+    void legendary() {
+        whenPotionIsConsumed(otherTower, PotionType.ChangeSex);
+
+        assertThat(tinker.getGender()).isEqualTo(Gender.Female);
+        assertThat(tinker.getLuck()).isEqualTo(1.0999999f);
+    }
+
+    @Test
+    void rarePotions() {
+        whenPotionIsConsumed(otherTower, PotionType.RareSpeed);
+        whenPotionIsConsumed(otherTower, PotionType.RareSpeed);
+        whenPotionIsConsumed(otherTower, PotionType.RareSpeed);
+        whenPotionIsConsumed(otherTower, PotionType.RareSpeed);
+
+        assertThat(tinker.getLuck()).isEqualTo(1.0659999f);
+    }
+}
