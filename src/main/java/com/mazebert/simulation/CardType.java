f2c7a3b45b8c006148ea0680bdabae7309c92cb5
Andreas Hager <andy@mazebert.com>
2020-04-13 07:58:03 +0200

    Add utility method to lookup card type by category and id

--- src/main/java/com/mazebert/simulation/CardType.java
+++ src/main/java/com/mazebert/simulation/CardType.java
@@ -1,6 +1,10 @@
 package com.mazebert.simulation;
 
 import com.mazebert.simulation.hash.Hashable;
+import com.mazebert.simulation.units.heroes.HeroType;
+import com.mazebert.simulation.units.items.ItemType;
+import com.mazebert.simulation.units.potions.PotionType;
+import com.mazebert.simulation.units.towers.TowerType;
 
 public interface CardType<T extends Card> extends Hashable {
     int id();
@@ -8,4 +12,20 @@ public interface CardType<T extends Card> extends Hashable {
     T create();
     @SuppressWarnings("unused") // Used by ui
     CardCategory category();
+
+
+    static CardType<?> forId(CardCategory cardCategory, int cardId) {
+        switch (cardCategory) {
+            case Tower:
+                return TowerType.forId(cardId);
+            case Item:
+                return ItemType.forId(cardId);
+            case Potion:
+                return PotionType.forId(cardId);
+            case Hero:
+                return HeroType.forId(cardId);
+        }
+
+        throw new IllegalStateException("Unknown card category " + cardCategory);
+    }
 }
