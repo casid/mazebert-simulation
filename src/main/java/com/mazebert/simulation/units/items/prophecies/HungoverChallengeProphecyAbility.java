6681666b7458727166917afb623e7718fdae1cfc
Andreas Hager <andy@mazebert.com>
2021-08-21 08:17:25 +0200

    Prophecy formatting

--- src/main/java/com/mazebert/simulation/units/items/prophecies/HungoverChallengeProphecyAbility.java
+++ src/main/java/com/mazebert/simulation/units/items/prophecies/HungoverChallengeProphecyAbility.java
@@ -3,11 +3,6 @@ package com.mazebert.simulation.units.items.prophecies;
 public strictfp class HungoverChallengeProphecyAbility extends ProphecyAbility {
 
     @Override
-    public String getTitle() {
-        return format.prophecyTitle("Drunk at work");
-    }
-
-    @Override
     public String getDescription() {
         return format.prophecyDescription("The next Challenge had a bad night at the pub and will start with 50% less health. The drunk Challenge will damage you, if it leaks.");
     }
--- src/main/java/com/mazebert/simulation/units/items/prophecies/WealthyBossProphecyAbility.java
+++ src/main/java/com/mazebert/simulation/units/items/prophecies/WealthyBossProphecyAbility.java
@@ -3,11 +3,6 @@ package com.mazebert.simulation.units.items.prophecies;
 public strictfp class WealthyBossProphecyAbility extends ProphecyAbility {
 
     @Override
-    public String getTitle() {
-        return format.prophecyTitle("Easy come, easy go");
-    }
-
-    @Override
     public String getDescription() {
         return format.prophecyDescription("The next Boss drops 2 additional cards but no gold.");
     }
