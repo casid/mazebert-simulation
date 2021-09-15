eea5ac466cf385e1368cb060013f969963401626
casid <andy@mazebert.com>
2021-09-15 21:11:42 +0200

    Reduce Hel armor penetration

--- src/main/java/com/mazebert/simulation/units/towers/HelAuraEffect.java
+++ src/main/java/com/mazebert/simulation/units/towers/HelAuraEffect.java
@@ -8,7 +8,7 @@ import com.mazebert.simulation.units.creeps.Creep;
 public strictfp class HelAuraEffect extends StackableByOriginAbility<Creep> implements OnDeathListener {
 
     public static float SPEED_MODIFIER = 0.7f;
-    public static int CREEPS_PER_ARMOR = 10;
+    public static int CREEPS_PER_ARMOR = 15;
 
     private int totalReduction;
     private float totalSpeedModifier = 1.0f;
