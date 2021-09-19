c9de12507ee1a2509c31f1309dae0d44a2a9f3dc
Andreas Hager <andy@mazebert.com>
2021-09-19 19:58:12 +0200

    Add concept draft for prophecy to unlock norse gods

---
+++ src/main/java/com/mazebert/simulation/units/items/prophecies/UnlockGodsProphecy.java
@@ -0,0 +1,13 @@
+package com.mazebert.simulation.units.items.prophecies;
+
+public strictfp class UnlockGodsProphecy {
+    // TODO Unique prophecy card
+    // Add hidden quests with progress for every norse god
+    // - Whenever a tower kills a challenge, it counts as sacrifice to the god of that tower's element
+    // - Quest progress is written on the card
+    // - On completion of the quest, the god's golden card is obtained!
+
+    // - If a god is already owned by the player, the god card is added to the players hand on the first sacrifice.
+
+    // TODO ensure Thor's Mjoelnir unlock is done properly on server side!
+}
