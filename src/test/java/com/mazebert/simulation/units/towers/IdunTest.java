fa6d9158623fa6df25edb5046202a1867153e00b
casid <andy@mazebert.com>
2021-10-03 20:15:30 +0200

    Add unit test for Idun

--- src/test/java/com/mazebert/simulation/units/towers/IdunTest.java
+++ src/test/java/com/mazebert/simulation/units/towers/IdunTest.java
@@ -1,7 +1,12 @@
 package com.mazebert.simulation.units.towers;
 
+import com.mazebert.simulation.units.creeps.Creep;
 import com.mazebert.simulation.units.items.ItemTest;
 import org.junit.jupiter.api.Test;
+
+import static com.mazebert.simulation.units.creeps.CreepBuilder.creep;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.jusecase.Builders.a;
 
 class IdunTest extends ItemTest {
 
@@ -11,8 +16,36 @@ class IdunTest extends ItemTest {
     }
 
     @Test
-    void name() {
-        // TODO creeps that die in range are revived
-        // TODO max level of nature towers in range is equal to your current health.
+    void reviveEffect() {
+        Creep creep = a(creep());
+        unitGateway.addUnit(creep);
+
+        assertThat(creep.hasAbility(IdunReviveAuraEffect.class)).isTrue();
+    }
+
+    @Test
+    void maxLevel_wizardHealthChanged() {
+        Tower rabbit = whenTowerNeighbourIsBuilt(this.tower, TowerType.Rabbit, 1, 0);
+        assertThat(rabbit.getMaxLevel()).isEqualTo(50);
+
+        wizard.addHealth(1.0f);
+        assertThat(rabbit.getMaxLevel()).isEqualTo(100);
+
+        wizard.addHealth(1.0f);
+        assertThat(rabbit.getMaxLevel()).isEqualTo(150);
+
+        wizard.addHealth(-1.0f);
+        assertThat(rabbit.getMaxLevel()).isEqualTo(100);
+    }
+
+    @Test
+    void maxLevel_idunSold() {
+        Tower rabbit = whenTowerNeighbourIsBuilt(this.tower, TowerType.Rabbit, 1, 0);
+        wizard.addHealth(1.0f);
+        assertThat(rabbit.getMaxLevel()).isEqualTo(100);
+
+        whenTowerIsSold();
+
+        assertThat(rabbit.getMaxLevel()).isEqualTo(99);
     }
 }
