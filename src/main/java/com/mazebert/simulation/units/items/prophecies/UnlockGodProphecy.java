9f03021c187d6efe9aa8765d0c483b06765ce957
casid <andy@mazebert.com>
2021-10-31 10:27:23 +0100

    Resolve todo

--- src/main/java/com/mazebert/simulation/units/items/prophecies/UnlockGodProphecy.java
+++ src/main/java/com/mazebert/simulation/units/items/prophecies/UnlockGodProphecy.java
@@ -9,16 +9,6 @@ import com.mazebert.simulation.units.quests.QuestType;
 import com.mazebert.simulation.units.towers.TowerType;
 
 public strictfp abstract class UnlockGodProphecy extends Prophecy {
-    // TODO Unique prophecy card
-    // Add hidden quests with progress for every norse god
-    // - Whenever a tower kills a challenge, it counts as sacrifice to the god of that tower's element
-    // - Quest progress is written on the card
-    // - On completion of the quest, the god's golden card is obtained!
-
-    // - If a god is already owned by the player, the god card is added to the players hand on the first sacrifice.
-
-    // TODO ensure Thor's Mjoelnir unlock is done properly on server side!
-
 
     public UnlockGodProphecy() {
         setAbilities(new UnlockGodProphecyAbility(getType(), getGod(), getQuest()));
