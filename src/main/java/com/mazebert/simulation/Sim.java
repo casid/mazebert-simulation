7f7ae1392cacdd18c19e2624eae3330ec7f1c47c
Andreas Hager <andy@mazebert.com>
2023-01-11 06:03:08 +0100

    Bump version to 30

--- src/main/java/com/mazebert/simulation/Sim.java
+++ src/main/java/com/mazebert/simulation/Sim.java
@@ -5,7 +5,7 @@ public strictfp class Sim {
      * If there are breaking changes, increment this number and the version in pom.xml
      * If there are no breaking changes, there is no need for a new version
      */
-    public static final int version = 29;
+    public static final int version = 30;
 
     public static final int v10 = 10;
     public static final int v11 = 11;
@@ -27,6 +27,7 @@ public strictfp class Sim {
     public static final int vHalloween = 27; // Halloween release
     public static final int vRnR = 28; // Rag nar Rog release
     public static final int vRnREnd = 29; // Rag nar Rog season end merge
+    public static final int v30 = 30;
 
     // For android 19 compatibility
     private static final ThreadLocal<Context> context = new ThreadLocal<Context>() {
